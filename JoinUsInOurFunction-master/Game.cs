d1c0ce8d65387dd80c24edb1818fcedd65920f7e
LukasGG1 <69813309+LukasGG1@users.noreply.github.com>
2020-09-16 09:24:15 -0500

    whys

--- JoinUsInOurFunction-master/Game.cs
+++ JoinUsInOurFunction-master/Game.cs
@@ -4,17 +4,10 @@ using System.Text;
 
 namespace HelloWorld
 {
-    struct Player
-    {
-        public string name;
-        public int health;
-        public int damage;
 
-    }
     struct Item
     {
         public int statBoost;
-        public int RestoreHealth;
     }
     class Game
     {
@@ -28,26 +21,17 @@ namespace HelloWorld
         {
             Start();
 
-            while (_gameOver == false) ;
+            while (_gameOver == false)
             {
                 Update();
             }
         
         End();
         }
-    
-        public void InitializePlayer()
-        {
-            //player1
-            player1.health = 100;
-            player1.damage = 5;
-            //player2
-            player2.health = 100;
-            player2.damage = 5;
-        }
         //Equip item to both players in the beginnning of the game
         public void GetInput(out char input, string option1, string option2, string query)
         {
+            //89 Year-old Man?
             Console.WriteLine(query);
             Console.WriteLine("1. " + option1);
             Console.WriteLine("2. " + option2);
@@ -62,25 +46,33 @@ namespace HelloWorld
                     Console.WriteLine("Invalid Input");
                 }
             }
+            //89 Year-old Man is in bus! We need get him!
             //Weapon Choose
             //---------------------------------------
-
+            //hELq
             input = Console.ReadKey().KeyChar;
         }
 
-        public void EquipItem()
+        public void EquipItem(Player player)
         {
             Console.WriteLine("Welcome to my blacksmith, newcomer! Please wisely choose a weapon might suit you.");
             Console.WriteLine("");
             Console.WriteLine("1. Long Sword");
             Console.WriteLine("2. Dagger");
+            Console.WriteLine("3. 89 Year-Old Man");
             //Weapon Choose
             //---------------------------------------
-
+            //wHY ArE WE HeRE SuuffEriNg?
             char input;
-            GetInput(out input, "Longsword", "Dagger", "Welcome to my blacksmith, newcomer! Please wisely choose a weapon might suit you.");
-
-            while(input != '1' && input != '2')
+            GetInput(out input, "Longsword", "Dagger", "Welcome to my blacksmith, newcomer 1! Please wisely choose a weapon might suit you.");
+            //HAVE YOU SEEN 89 YEAR-OLD MAN?
+            //HAVE YOU SEEN 89 YEAR-OLD MAN?
+            //HAVE YOU SEEN 89 YEAR-OLD MAN?
+            //HAVE YOU SEEN 89 YEAR-OLD MAN?
+            //HAVE YOU SEEN 89 YEAR-OLD MAN?
+            //HAVE YOU SEEN 89 YEAR-OLD MAN?
+            //HAVE YOU SEEN 89 YEAR-OLD MAN?
+            while (input != '1' && input != '2')
             {
                 input = Console.ReadKey().KeyChar;
                 if (input == '1')
@@ -91,7 +83,36 @@ namespace HelloWorld
                 {
                     player1.damage += dagger.statBoost;
                 }
+                Console.WriteLine("Player 1");
+                player1.PrintStat();
+
+                Console.Clear();
+
+                GetInput(out input, "Longsword", "Dagger", "Welcome to my blacksmith, newcomer 2! Please wisely choose a weapon might suit you.");
+
+                while (input != '1' && input != '2')
+                {
+                    input = Console.ReadKey().KeyChar;
+                    if (input == '1')
+                    {
+                        player2.damage += longSword.statBoost;
+                    }
+                    else if (input == '2')
+                    {
+                        player2.damage += dagger.statBoost;
+                    }
+                    Console.WriteLine("Player 2");
+                    player2.PrintStat();
+                }
             }
+        }
+
+        public void CreateCharacter(Player player)
+        {
+            Console.WriteLine("What is your name, mortal?");
+            string name = Console.ReadLine();
+            player = new Player(name, 100, 10);
+            EquipItem(player);
         }
 
         public void InitializeItem()
@@ -99,17 +120,75 @@ namespace HelloWorld
             longSword.statBoost = 15;
             dagger.statBoost = 10;
         }
+
+        public void StartBattle()
+        {
+            Console.Clear();
+            Console.WriteLine("Fight to death!");
+
+            while (player1.GetIsAlive() && player2.GetIsAlive())
+            {
+                Console.WriteLine("Player 1");
+                player1.PrintStat();
+                Console.WriteLine("Player 2");
+                player2.PrintStat();
+                //Player 1 turn start
+                char input;
+                GetInput(out input, "Attack", "No", "Your Turn, Player 1");
+
+                if(input == '1')
+                {
+
+                    player2.Attack(player1);
+
+                }
+                else
+                {
+                    Console.WriteLine("None");
+                }
+
+                GetInput(out input, "Attack", "No", "Your Turn, Player 2");
+
+                if (input == '1')
+                {
+                    player1.health -= player2.damage;
+                    Console.WriteLine("Player 1 took " + player2.damage + " damage");
+
+                }
+                else
+                {
+                    Console.WriteLine("None");
+                }
+                Console.Clear();
+            }
+            if (player1.GetIsAlive())
+            {
+                Console.WriteLine("Player 1 Won");
+            }
+            else
+            {
+                Console.WriteLine("Player 2 Won");
+            }
+            Console.Clear();
+            _gameOver = true;
+        }
+
         //Performed once when the game begins
         public void Start()
         {
-            InitializePlayer();
+
+            //InitializePlayer();
             InitializeItem();
         }
 
         //Repeated until the game ends
         public void Update()
         {
-            
+            CreateCharacter(player1);
+            CreateCharacter(player2);
+            EquipItem(player1);
+            EquipItem(player2);
+            StartBattle();
         }
 
         //Performed once when the game ends
